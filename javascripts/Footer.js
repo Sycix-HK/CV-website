cd122455bcac455d64d43798e6992301035342fd
Sycix-HK <warriorkathun@gmail.com>
2021-12-13 21:45:29 +0100

    Added contact and footer

--- javascripts/Footer.js
+++ javascripts/Footer.js
@@ -1,14 +1,14 @@
 const buttons = [
     {
-        elementId: '#f-phone',
+        elementId: 'f-phone',
         text: "+36 30 232 7022"
     },
     {
-        elementId: '#f-email',
+        elementId: 'f-email',
         text: "hoffmann.kristof00@gmail.com"
     },
     {
-        elementId: '#f-discord',
+        elementId: 'f-discord',
         text: "Sycix#4674"
     }
 ]
@@ -25,16 +25,11 @@ buttons.map(button => {
             feedback = window.location.pathname.endsWith("/hu/") ? "Hibás másolás" : "Copy failed"
         }
         let p = document.createElement('p')
-        document.getElementById(button.elementId).appendChild(p)
+        document.getElementById(button.elementId).childNodes[1].appendChild(p)
+        p.classList.add("feedback")
         p.style.transition = "1.1s",
         p.style.ease = 'easeOutCubic'
         p.innerText = feedback
-        p.style.transform = `translateY(-2vh)`
-        p.style.opacity = 0
-        p.style.pointerEvents = 'none'
-        p.style.marginLeft = 'auto'
-        p.style.marginRight = 'auto'
-        p.style.position = 'absolute'
         setTimeout(() => {
             p.style.transform = `translateY(-7vh)`
             p.style.opacity = 100;
