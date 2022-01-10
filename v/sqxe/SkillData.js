162edbfb8dec00ff400e33f1d8899595431d9a46
Sycix-HK <warriorkathun@gmail.com>
2022-01-10 23:56:40 +0100

    variant created

---
+++ v/sqxe/SkillData.js
@@ -0,0 +1,42 @@
+const skills = {
+    languages: [
+        { name: "JavaScript", imgpath: "JavaScript.svg", 
+        level: 5    },
+        { name: "Java", imgpath: "Java.jpg", 
+        level: 6    },
+        { name: "Haskell", imgpath: "Haskell.svg", 
+        level: 2    },
+        { name: "C# / .NET", imgpath: "Csharp.svg", 
+        level: 4    },
+        { name: "C++", imgpath: "Cpp.png", 
+        level: 3    },
+        { name: "C", imgpath: "C.png", 
+        level: 2    },
+        { name: "Powershell", imgpath: "Powershell.jpg", 
+        level: 2    },
+        { name: "Bash", imgpath: "Bash.svg", 
+        level: 2    },
+        { name: "CSS", imgpath: "css.png", 
+        level: 5    },
+        { name: "Sass", imgpath: "Sass.svg", 
+        level: 5    },
+        { name: "HTML", imgpath: "html.png", 
+        level: 6    },
+        { name: "PHP", imgpath: "php.svg", 
+        level: 4    },
+        { name: "Python", imgpath: "python.svg", 
+        level: 2    }
+    ],
+    frameworks: [
+        { name: "jQuery", imgpath: "jquery.svg", 
+        level: 2    },
+        { name: "ReactJS", imgpath: "react.svg", 
+        level: 1    }
+    ],
+    graphics: [
+        { name: "Photoshop", imgpath: "Photoshop.png", 
+        level: 6    },
+        { name: "Blender", imgpath: "Blender.png", 
+        level: 4    }
+    ]
+}
