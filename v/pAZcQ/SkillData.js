37d5807447ded60f08f93e60a6d08fc792bfb772
Sycix-HK <warriorkathun@gmail.com>
2022-01-22 17:08:36 +0100

    added version for Paradigm

---
+++ v/pAZcQ/SkillData.js
@@ -0,0 +1,18 @@
+const skills = {
+    languages: [
+        { name: "C# / .NET", imgpath: "Csharp.svg", 
+        level: 6    },
+        { name: "C++", imgpath: "Cpp.png", 
+        level: 4    },
+        { name: "C", imgpath: "C.png",
+        level: 3    },
+        { name: "Java", imgpath: "Java.jpg", 
+        level: 5    }
+    ],
+    frameworks: [
+
+    ],
+    graphics: [
+
+    ]
+}
